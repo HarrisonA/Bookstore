35d4584f4346343d715afc2287b7814b5b931d37
HarrisonA <radiov@gmail.com>
2016-08-01 19:13:24 -0700

    BlurredBackground now works in Bio view when a book is clicked

--- Bookstore-FrontEnd/app/routes/author.js
+++ Bookstore-FrontEnd/app/routes/author.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params){
     return this.store.findRecord('author', params.author_id);  //author_id comes
+  },
+
+  actions: {
+    blurBackground(blur) {
+      this.controllerFor('application').set('blur', blur);
+    }
   }
 });
